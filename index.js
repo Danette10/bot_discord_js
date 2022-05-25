d630761a556d4c30b5d54aa8ed61e487015f51ca
Danette10 <dan.sebag1007@gmail.com>
2022-05-25 11:13:09 +0200

    update commands clear with permissions

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on('interactionCreate', async interaction => {
         await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
     } else if (commandName === 'user') {
         await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-    }else if(commandName === 'clear'){
+    }else if(commandName === 'clear' && interaction.member.permissions.has('MANAGE_MESSAGES')){
         let number = interaction.options.getInteger('number');
         if(number >= 1 && number <= 100){
             interaction.channel.messages.fetch({limit: number}).then(messages => {
@@ -31,9 +31,12 @@ client.on('interactionCreate', async interaction => {
         }else{
             interaction.reply({content: 'Veuillez entrer un nombre entre 1 et 100 !', ephemeral: true});
         }
+        // delete Reply after 5 seconds
+        setTimeout(() => interaction.deleteReply(), 5000);
+    }else{
+        interaction.reply({content: '**' + interaction.user.username + '**' + ', vous n\'avez pas la permission de supprimer des messages !', ephemeral: false});
     }
-    // delete Reply after 5 seconds
-    setTimeout(() => interaction.deleteReply(), 5000);
+
 });
 
 // Login to Discord with your client's token
