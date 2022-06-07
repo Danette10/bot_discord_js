2e7b73ee7681e5eee97137dbf904e3757967f4c3
Danette10 <dan.sebag1007@gmail.com>
2022-06-07 14:45:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -155,10 +155,13 @@ client.on('interactionCreate', async interaction => {
     }
 
     else if(commandName === 'list'){
+
         let members = interaction.guild.members.cache.map(member => member.user.tag);
+        
         let membersString = members.join('\n');
 
         let embedList = new MessageEmbed()
+
             .setTitle(`Liste des membres du serveur ${interaction.guild.name}`)
             .setDescription(membersString.split('\n').join('\n\n'))
             .setThumbnail(interaction.guild.iconURL())
