3f35f562d583444021f46e0fcd20ac57f75e7650
Danette10 <dan.sebag1007@gmail.com>
2022-06-13 14:28:17 +0200

    Update bestjustprice.js

--- commands/games/statgames/bestjustprice.js
+++ commands/games/statgames/bestjustprice.js
@@ -1,7 +1,7 @@
 const Enmap = require("enmap");
 module.exports = interaction => {
     interaction.justprice = new Enmap({ name: "justprice" });
-    let justpriceMember = interaction.justprice.get(interaction.user.tag);
+    let justpriceMember = interaction.justprice.get(`${interaction.guild.name}-${interaction.user.tag}`);
     if(justpriceMember === undefined){
         interaction.reply({
             content: "Vous n'avez pas encore jouer au juste prix ! Vous pouvez en commencer une en tapant la commande `/justprice`",
