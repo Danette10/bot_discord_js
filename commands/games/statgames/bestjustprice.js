0a4f4c1aee7e24a0c7892bee9426443aacc466a6
Danette10 <dan.sebag1007@gmail.com>
2022-06-13 14:40:17 +0200

    up

--- commands/games/statgames/bestjustprice.js
+++ commands/games/statgames/bestjustprice.js
@@ -1,7 +1,7 @@
 const Enmap = require("enmap");
 module.exports = interaction => {
     const result = new Enmap({name: 'justprice'});
-    let justpriceMember = result.get(`${interaction.guild.name}-${interaction.user.tag}`);
+    let justpriceMember = result.get(`${interaction.user.id}-${interaction.guild.name}-${interaction.user.tag}`);
     if(justpriceMember === undefined){
         interaction.reply({
             content: "Vous n'avez pas encore jouer au juste prix ! Vous pouvez en commencer une en tapant la commande `/justprice`",
--- commands/games/statgames/winpendu.js
+++ commands/games/statgames/winpendu.js
@@ -1,7 +1,7 @@
 const Enmap = require("enmap");
 module.exports = interaction => {
     const result = new Enmap({name: 'pendu'});
-    let penduMember = result.get(`${interaction.guild.name}-${interaction.user.tag}`);
+    let penduMember = result.get(`${interaction.user.id}-${interaction.guild.name}-${interaction.user.tag}`);
     if(penduMember === undefined){
         interaction.reply({
             content: "Vous n'avez pas encore commencer de partie ! Vous pouvez en commencer une en tapant la commande `/pendu`",
